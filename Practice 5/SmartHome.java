6ca507fd8dadf78aa8c36c394eb8a20d8765e3b0
Art Vincent D. Papolonias <artvincent.papolonias@g.msuiit.edu.ph>
2026-02-18 19:01:02 +0800

    final practice before i head to Encapsulation!

---
+++ Practice 5/SmartHome.java
@@ -0,0 +1,40 @@
+class LightBulb{
+    String roomName;
+    boolean isOn;
+
+    //Constructor
+    LightBulb(String roomName, boolean isOn) {
+        this.roomName = roomName;
+        this.isOn = false;
+    }
+
+    void switchOn() {
+        isOn = true;
+        System.out.println("The " + roomName + " light is now ON.");
+    }
+
+    void switchOff(){
+        isOn = true;
+        System.out.println("The " + roomName + " light is now Off");
+    }
+
+    void status(){
+        if (isOn = true){
+        System.out.println("The " + roomName + " is on");
+        } else {
+            System.out.println("The " + roomName + " is Off");
+        }
+    }
+}
+
+public class SmartHome {
+    public static void main(String[] args) {
+        
+    
+    LightBulb room1 = new LightBulb("Room 1", true);
+
+    room1.switchOff();
+    room1.switchOn();
+    room1.status();
+    }
+}
