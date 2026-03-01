c44f43227081c2428c4453e5ffe164375a718c03
Art Vincent D. Papolonias <artvincent.papolonias@g.msuiit.edu.ph>
2026-03-01 14:05:25 +0800

    final variable and method.

--- Practice23/Main.java
+++ Practice23/Main.java
@@ -9,10 +9,11 @@ public class Main {
         SecuritySystem sec = new SecuritySystem("1234");
 
         System.out.println("MasterKey: " + sec.master_key);
+        sec.rebootSystem();
         
         //Doing this would cause an error.
         //Cannot assign a value to a final variable
-        //sec.master_key = "Love";
+        //sec.master_key = "0123";
 
 
     }
